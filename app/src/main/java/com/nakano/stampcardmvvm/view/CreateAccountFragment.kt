9b1fcc686891a7ba82930d6e06ea99553cf6825b
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-03 11:56:47 +0900

    rename LoginFragment.kt -> EmailLoginFragment.kt

--- app/src/main/java/com/nakano/stampcardmvvm/view/CreateAccountFragment.kt
+++ app/src/main/java/com/nakano/stampcardmvvm/view/CreateAccountFragment.kt
@@ -7,14 +7,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
 import com.nakano.stampcardmvvm.R
 import com.nakano.stampcardmvvm.databinding.FragmentCreateAccountBinding
-import com.nakano.stampcardmvvm.databinding.FragmentLoginBinding
 import com.nakano.stampcardmvvm.viewModel.StampCardViewModel
 import com.nakano.stampcardmvvm.viewModel.StampCardViewModelFactory
-import kotlinx.android.synthetic.main.fragment_login.*
-import kotlinx.android.synthetic.main.fragment_stamp_card.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
--- app/src/main/java/com/nakano/stampcardmvvm/view/EmailLoginFragment.kt
+++ app/src/main/java/com/nakano/stampcardmvvm/view/EmailLoginFragment.kt
@@ -9,16 +9,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.nakano.stampcardmvvm.R
-import com.nakano.stampcardmvvm.databinding.FragmentLoginBinding
+import com.nakano.stampcardmvvm.databinding.FragmentEmailLoginBinding
 import com.nakano.stampcardmvvm.viewModel.StampCardViewModel
 import com.nakano.stampcardmvvm.viewModel.StampCardViewModelFactory
-import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_email_login.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
 
 
-class LoginFragment : Fragment(), KodeinAware {
+class EmailLoginFragment : Fragment(), KodeinAware {
 
     override val kodein by kodein()
     private val factory: StampCardViewModelFactory by instance()
@@ -29,8 +29,8 @@ class LoginFragment : Fragment(), KodeinAware {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentLoginBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        val binding: FragmentEmailLoginBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_email_login, container, false)
         viewModel = ViewModelProviders.of(this, factory).get(StampCardViewModel::class.java)
         binding.stampCardViewModel = viewModel
         binding.lifecycleOwner = this
@@ -41,20 +41,17 @@ class LoginFragment : Fragment(), KodeinAware {
         super.onActivityCreated(savedInstanceState)
 
         login_button.setOnClickListener {
-//            val action =
-//                LoginFragmentDirections.actionLoginFragmentToCreateAccountFragment()
-//            findNavController().navigate(action)
         }
 
         forgot_password.setOnClickListener {
             val action =
-                LoginFragmentDirections.actionLoginFragmentToChangePasswordFragment()
+                EmailLoginFragmentDirections.actionLoginFragmentToChangePasswordFragment()
             findNavController().navigate(action)
         }
 
         new_registration.setOnClickListener {
             val action =
-                LoginFragmentDirections.actionLoginFragmentToCreateAccountFragment()
+                EmailLoginFragmentDirections.actionLoginFragmentToCreateAccountFragment()
             findNavController().navigate(action)
         }
     }
