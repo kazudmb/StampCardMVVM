45b0f434291363ae7d9f9344c2619145d4c3c195
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-04 15:11:59 +0900

    add navigation StampCardFragment to LoginFragment

---
+++ app/src/main/java/com/nakano/stampcardmvvm/view/LoginFragment.kt
@@ -0,0 +1,56 @@
+package com.nakano.stampcardmvvm.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.nakano.stampcardmvvm.R
+import com.nakano.stampcardmvvm.databinding.FragmentLoginBinding
+import com.nakano.stampcardmvvm.viewModel.StampCardViewModel
+import com.nakano.stampcardmvvm.viewModel.StampCardViewModelFactory
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.kodein
+import org.kodein.di.generic.instance
+
+// TODO: login画面を実装する（いろんなサービス連携できるように）
+class LoginFragment : Fragment(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: StampCardViewModelFactory by instance()
+
+    private lateinit var viewModel: StampCardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentLoginBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        viewModel = ViewModelProviders.of(this, factory).get(StampCardViewModel::class.java)
+        binding.stampCardViewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        login_button.setOnClickListener {
+//        }
+//
+//        forgot_password.setOnClickListener {
+//            val action =
+//                EmailLoginFragmentDirections.actionLoginFragmentToChangePasswordFragment()
+//            findNavController().navigate(action)
+//        }
+//
+//        new_registration.setOnClickListener {
+//            val action =
+//                EmailLoginFragmentDirections.actionLoginFragmentToCreateAccountFragment()
+//            findNavController().navigate(action)
+//        }
+    }
+}
