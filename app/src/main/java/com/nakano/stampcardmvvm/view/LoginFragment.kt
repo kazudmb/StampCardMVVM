be5bbb8fc0d3ffab32edd848a651832eed001b2f
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-04 15:18:29 +0900

    add navigation LoginFragment to EmailLoginFragment

--- app/src/main/java/com/nakano/stampcardmvvm/view/LoginFragment.kt
+++ app/src/main/java/com/nakano/stampcardmvvm/view/LoginFragment.kt
@@ -7,10 +7,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.nakano.stampcardmvvm.R
 import com.nakano.stampcardmvvm.databinding.FragmentLoginBinding
 import com.nakano.stampcardmvvm.viewModel.StampCardViewModel
 import com.nakano.stampcardmvvm.viewModel.StampCardViewModelFactory
+import kotlinx.android.synthetic.main.fragment_login.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
@@ -38,19 +40,10 @@ class LoginFragment : Fragment(), KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-//        login_button.setOnClickListener {
-//        }
-//
-//        forgot_password.setOnClickListener {
-//            val action =
-//                EmailLoginFragmentDirections.actionLoginFragmentToChangePasswordFragment()
-//            findNavController().navigate(action)
-//        }
-//
-//        new_registration.setOnClickListener {
-//            val action =
-//                EmailLoginFragmentDirections.actionLoginFragmentToCreateAccountFragment()
-//            findNavController().navigate(action)
-//        }
+        login_email.setOnClickListener {
+            val action =
+                LoginFragmentDirections.actionLoginFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
     }
 }
