630ad7657b873aab37e749b1bd4a2b43dc28e5ec
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-13 15:06:50 +0900

    update data class

--- app/src/main/java/com/nakano/stampcardmvvm/model/model/UserFirebase.kt
+++ app/src/main/java/com/nakano/stampcardmvvm/model/model/UserFirebase.kt
@@ -1,17 +1,9 @@
 package com.nakano.stampcardmvvm.model.model
 
-import com.google.firebase.firestore.Exclude
-
 data class UserFirebase(
     var uid: String? = null,
     var name: String? = null,
     var email: String? = null,
     var numberOfVisits: String? = null,
-    var rank: String? = null,
-
-    @Exclude
-    var isAuthenticated: Boolean? = false,
-
-    @Exclude
-    var isCreated: Boolean? = false
+    var rank: String? = null
 )
