051136c661403f321939f4d34308a9af80488ba9
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-15 17:15:25 +0900

    add TODO comment

--- app/src/main/java/com/nakano/stampcardmvvm/model/model/User.kt
+++ app/src/main/java/com/nakano/stampcardmvvm/model/model/User.kt
@@ -1,9 +1,10 @@
 package com.nakano.stampcardmvvm.model.model
 
+// TODO: data classの初期値はどのような内容が推奨か調査して実装
 data class User(
     var uid: String? = null,
     var name: String? = null,
     var email: String? = null,
-    var numberOfVisits: String? = null,
+    var numberOfVisits: String? = null, // TODO: High Intで保存したい
     var rank: String? = null
 )
